dd80ef0bdbacc8b4f3467a59bc974bf0736af701
AgriDulce7u7 <142194231+AgriDulce7u7@users.noreply.github.com>
2023-11-23 15:07:06 -0500

    tienda temporal

---
+++ co.edu.uniquindio.tiendaDeRopa/src/main/java/co/edu/uniquindio/tiendaDeRopa/Model/Tiendamomentanea.java
@@ -0,0 +1,512 @@
+package co.edu.uniquindio.tiendaDeRopa.Model;
+
+import co.edu.uniquindio.tiendaDeRopa.Model.Enumeracion.*;
+import co.edu.uniquindio.tiendaDeRopa.Service.ITienda;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Tiendamomentanea implements ITienda {
+    private  String nombre;
+    List<Cliente> listaClientes = new ArrayList<>();
+    List<Empleado> listaEmpleados = new ArrayList<>();
+    List<Compra> listaCompras = new ArrayList<>();
+    List<Producto> listaProductos = new ArrayList<>();
+    List<DetalleCompra> listaDetalleCompra = new ArrayList<>();
+
+    /*Constructor*/
+    public Tiendamomentanea() {
+    }
+
+    /*Getters and Setters*/
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+
+    public void setListaClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public List<Empleado> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(List<Empleado> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    public List<DetalleCompra> getListaDetalleCompra() {
+        return listaDetalleCompra;
+    }
+
+    public void setListaDetalleCompra(List<DetalleCompra> listaDetalleCompra) {
+        this.listaDetalleCompra = listaDetalleCompra;
+    }
+
+    public List<Compra> getListaCompras() {
+        return listaCompras;
+    }
+
+    public void setListaCompras(List<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    @Override
+    public String toString() {
+        return "Tienda{" +
+                "Nombre = " + nombre + '\'' +
+                '}';
+    }
+
+    /* Método para crear un cliente */
+    @Override
+    public boolean crearCliente(String nombreCompleto, String cedula, Sexo sexo, long telefono, int prendasCompradas, Date fechaUltimaCompra, MetodoPago metodoPago, String codigoCompra) {
+        if (buscarClientes(cedula) == null){
+            Cliente cliente = new Cliente();
+            cliente.setNombreCompleto(nombreCompleto);
+            cliente.setCedula(cedula);
+            cliente.setSexo(sexo);
+            cliente.setTelefono(telefono);
+            cliente.setPrendasCompradas(prendasCompradas);
+            cliente.setFechaUltimaCompra(fechaUltimaCompra);
+            cliente.setMetodoPago(metodoPago);
+            getListaClientes().add(cliente);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Cliente obtenerCliente(String cedula) {
+        Cliente clienteEncontrado = null;
+        for (Cliente cliente : getListaClientes()) {
+            if (cliente.getCedula().equalsIgnoreCase(cedula)){
+                clienteEncontrado = cliente;
+                break;
+            }
+        }
+
+        return clienteEncontrado;
+    }
+
+    @Override
+    public boolean actualizarCliente(String cedulaActual, String nombreCompleto, String cedula, Sexo sexo, long telefono, int prendasCompradas, Date fechaUltimaCompra, MetodoPago metodoPago, String codigoCompra) {
+        Cliente clienteEncontrado = obtenerCliente(cedulaActual);
+        if(clienteEncontrado != null){
+            clienteEncontrado.setNombreCompleto(nombreCompleto);
+            clienteEncontrado.setCedula(cedula);
+            clienteEncontrado.setSexo(sexo);
+            clienteEncontrado.setTelefono(telefono);
+            clienteEncontrado.setPrendasCompradas(prendasCompradas);
+            clienteEncontrado.setFechaUltimaCompra(fechaUltimaCompra);
+            clienteEncontrado.setMetodoPago(metodoPago);
+            getListaClientes().add(clienteEncontrado);
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void eliminarCliente(String cedula) {
+        Cliente clienteEncontrado = obtenerCliente(cedula);
+        if (clienteEncontrado != null){
+            System.out.println("El cliente fue eliminado.");
+            getListaClientes().remove(clienteEncontrado);
+        }else{
+            System.out.println("El cliente no pudo ser eliminado.");
+        }
+    }
+
+    /**
+     * Metodo para buscar y mostrar la informacion de un cliente
+     * @param cedula
+     */
+    public void buscarCliente(String cedula) {
+        Cliente clienteEncontrado = obtenerCliente(cedula);
+        if(clienteEncontrado != null){
+            System.out.println("El cliente fue encontrado con éxito.");
+            System.out.println(clienteEncontrado.toString());
+        }else{
+            System.out.println("El cliente no fue encontraddo.");
+        }
+    }
+
+    public Cliente buscarClientes(String cedula) {
+        for (Cliente cliente : getListaClientes()) {
+            if (cliente.getCedula().equalsIgnoreCase(cedula)){
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    public int obtenerClienteConMasCompras() {
+        int clienteMayorCompras = 0;
+        for (Cliente cliente : getListaClientes()) {
+            if (cliente.getPrendasCompradas() > clienteMayorCompras){
+                clienteMayorCompras = cliente.getPrendasCompradas();
+            }
+            System.out.println("El cliente con más prendas compradas es: "+ clienteMayorCompras);
+            break;
+        }
+        return clienteMayorCompras;
+    }
+
+    public String mostrarClienteConMasComprasAsociadas(){
+        int compras = 0;
+        Cliente clienteMasCompras = null;
+        for (Cliente cliente : getListaClientes()) {
+            if (cliente.getComprasAsociadas().size() > compras){
+                compras = cliente.getComprasAsociadas().size();
+                clienteMasCompras = cliente;
+            }
+        }
+        String mensaje = "El cliente con más compras es: \n"+
+                clienteMasCompras.toString()+"\n"+
+                "El número de compras hechas por este cliente es: "+ compras;
+        return mensaje;
+    }
+
+    @Override
+    public boolean crearEmpleado(String nombreCompleto, String cedula, Sexo sexo, long telefono, String correo, double salario, TipoContrato tipoContrato, int horasTrabajo, String codigoCompra) {
+        if (buscarEmpleados(cedula) == null){
+            Empleado empleado = new Empleado();
+            empleado.setNombreCompleto(nombreCompleto);
+            empleado.setCedula(cedula);
+            empleado.setSexo(sexo);
+            empleado.setTelefono(telefono);
+            empleado.setCorreo(correo);
+            empleado.setSalario(salario);
+            empleado.setTipoContrato(tipoContrato);
+            empleado.setHorasTrabajo(horasTrabajo);
+            getListaEmpleados().add(empleado);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Empleado obtenerEmpleado(String cedula) {
+        Empleado empleadoEncontrado = null;
+        for (Empleado empleado : getListaEmpleados()) {
+            if (empleado.getCedula().equalsIgnoreCase(cedula)){
+                empleadoEncontrado = empleado;
+                break;
+            }
+        }
+
+        return empleadoEncontrado;
+    }
+
+    @Override
+    public boolean actualizarEmpleado(String cedulaActual, String nombreCompleto, String cedula, Sexo sexo, long telefono, String correo, double salario, TipoContrato tipoContrato, int horasTrabajo, String codigoCompra) {
+        Empleado empleadoEncontrado = obtenerEmpleado(cedulaActual);
+        if(empleadoEncontrado != null){
+            empleadoEncontrado.setNombreCompleto(nombreCompleto);
+            empleadoEncontrado.setCedula(cedula);
+            empleadoEncontrado.setSexo(sexo);
+            empleadoEncontrado.setTelefono(telefono);
+            empleadoEncontrado.setCorreo(correo);
+            empleadoEncontrado.setSalario(salario);
+            empleadoEncontrado.setTipoContrato(tipoContrato);
+            empleadoEncontrado.setHorasTrabajo(horasTrabajo);
+            getListaEmpleados().add(empleadoEncontrado);
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void eliminarEmpleado(String cedula) {
+        Empleado empleadoEncontrado = obtenerEmpleado(cedula);
+        if (empleadoEncontrado != null){
+            System.out.println("El empleado fue eliminado.");
+            getListaEmpleados().remove(empleadoEncontrado);
+        }else{
+            System.out.println("El empleado no pudo ser eliminado.");
+        }
+    }
+
+    public void buscarEmpleado(String cedula) {
+        Empleado empleadoEncontrado = obtenerEmpleado(cedula);
+        if(empleadoEncontrado != null){
+            System.out.println("El empleado fue encontrado con éxito.");
+            System.out.println(empleadoEncontrado.toString());
+        }else{
+            System.out.println("El empleado no fue encontraddo.");
+        }
+    }
+
+    public Empleado buscarEmpleados(String cedula) {
+        for (Empleado empleado : getListaEmpleados()) {
+            if (empleado.getCedula().equalsIgnoreCase(cedula)){
+                return empleado;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean crearProducto(String nombre, String referencia, TipoCliente tipoCliente, TipoProducto tipoProducto, Talla talla, Color color, double precio, int cantidadDisponible, int numeroDetalle) {
+        if (buscarProductos(referencia) == null) {
+            Producto producto = new Producto();
+            producto.setNombre(nombre);
+            producto.setReferencia(referencia);
+            producto.setTipoCliente(tipoCliente);
+            producto.setTipoProducto(tipoProducto);
+            producto.setTalla(talla);
+            producto.setColor(color);
+            producto.setPrecio(precio);
+            producto.setCantidadDisponible(cantidadDisponible);
+            getListaProductos().add(producto);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public Producto obtenerProducto(String referencia) {
+        Producto productoEncontrado = null;
+        for (Producto producto : getListaProductos()) {
+            if (producto.getReferencia().equalsIgnoreCase(referencia)){
+                productoEncontrado = producto;
+                break;
+            }
+        }
+
+        return productoEncontrado;
+    }
+
+    @Override
+    public boolean actualizarProducto(String referenciaActual, String nombre, String referencia, TipoCliente tipoCliente, TipoProducto tipoProducto, Talla talla, Color color, double precio, int cantidadDisponible, int numeroDetalle) {
+        Producto productoEncontrado = obtenerProducto(referenciaActual);
+        if(productoEncontrado != null){
+            productoEncontrado.setNombre(nombre);
+            productoEncontrado.setReferencia(referencia);
+            productoEncontrado.setTipoCliente(tipoCliente);
+            productoEncontrado.setTipoProducto(tipoProducto);
+            productoEncontrado.setTalla(talla);
+            productoEncontrado.setColor(color);
+            productoEncontrado.setPrecio(precio);
+            productoEncontrado.setCantidadDisponible(cantidadDisponible);
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void eliminarProducto(String referencia) {
+        Producto productoEncontrado = obtenerProducto(referencia);
+        if (productoEncontrado != null){
+            System.out.println("El producto fue eliminado.");
+            getListaProductos().remove(productoEncontrado);
+        }else{
+            System.out.println("El producto no pudo ser eliminado.");
+        }
+    }
+
+    public boolean buscarProducto(String referencia) {
+        Producto productoEncontrado = obtenerProducto(referencia);
+        if(productoEncontrado != null){
+            System.out.println("El producto fue encontrado con éxito.");
+            System.out.println(productoEncontrado.toString());
+            return true;
+        }else{
+            System.out.println("El producto no fue encontraddo.");
+            return false;
+        }
+    }
+
+    public Producto buscarProductos(String referencia) {
+        for (Producto producto : getListaProductos()) {
+            if (producto.getReferencia().equalsIgnoreCase(referencia)){
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean crearCompra(String codigoCompra, Date fechaCompra, double totalCompra, String cedulaCliente, String cedulaEmpleado) {
+        if (buscarCompra(codigoCompra) == null) {
+            Cliente cliente = obtenerCliente(cedulaCliente);
+            Empleado empleado = obtenerEmpleado(cedulaEmpleado);
+            if (cliente == null || empleado == null){
+                return false;
+            }else{
+                Compra compra = new Compra();
+                compra.setCodigoCompra(codigoCompra);
+                compra.setFechaCompra(fechaCompra);
+                compra.setTotalCompra(totalCompra);
+                compra.setClienteAsociado(cliente);
+                compra.setEmpleadoAsociado(empleado);
+                compra.crearDetalleCompra();
+                getListaCompras().add(compra);
+                return true;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public Compra obtenerCompra(String codigoCompra) {
+        Compra compraEncontrada = null;
+        for (Compra compra : getListaCompras()) {
+            if (compra.getCodigoCompra().equalsIgnoreCase(codigoCompra)){
+               compraEncontrada = compra;
+                break;
+            }
+        }
+
+        return compraEncontrada;
+    }
+
+    @Override
+    public boolean actualizarCompra(String codigoCompraActual, String codigoCompra, Date fechaCompra, double totalCompra, String cedulaCliente, String cedulaEmpleado) {
+//        Compra compraEncontrada = obtenerCompra(codigoCompraActual);
+//        if(compraEncontrada != null){
+//            compraEncontrada.setCodigoCompra(codigoCompra);
+//            compraEncontrada.setFechaCompra(fechaCompra);
+//            compraEncontrada.setTotalCompra(totalCompra);
+//
+//            return true;
+//        }else{
+//            return false;
+//        }
+        Empleado empleadoAsociado;
+        Cliente clienteAsociado;
+
+        if (obtenerCompra(codigoCompra) == null || codigoCompra == codigoCompraActual){
+            empleadoAsociado = obtenerEmpleado(cedulaEmpleado);
+            clienteAsociado = obtenerCliente(cedulaCliente);
+
+            if (empleadoAsociado == null || clienteAsociado == null){
+                return false;
+
+            }else{
+                Compra compra = obtenerCompra(codigoCompra);
+                compra.setCodigoCompra(codigoCompra);
+                compra.setFechaCompra(fechaCompra);
+                compra.setTotalCompra(totalCompra);
+                compra.setClienteAsociado(clienteAsociado);
+                compra.setEmpleadoAsociado(empleadoAsociado);
+                compra.crearDetalleCompra(cantidad, fechaCompra, idProductoAsociado);
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public void eliminarCompra(String codigoCompra) {
+    }
+
+    public Compra buscarCompra(String codigoCompra){
+        for (Compra compra : getListaCompras()) {
+            if (compra.getCodigoCompra().equalsIgnoreCase(codigoCompra)){
+                return compra;
+            }
+        }
+        return null;
+    }
+
+
+    @Override
+    public boolean crearDetalleCompra(int numeroDetalle, String productoComprado, int cantidadCompra, double valorUnidad, double valorTotal, String codigoCompra, String referencia) {
+        return false;
+    }
+
+    @Override
+    public DetalleCompra obtenerDetalleCompra(int numeroDetalle) {
+        DetalleCompra detalleCompraEncontrado = null;
+        for (DetalleCompra detalleCompra : getListaDetalleCompra()) {
+            if (detalleCompra.getNumeroDetalle() == numeroDetalle){
+                detalleCompraEncontrado = detalleCompra;
+                break;
+            }
+        }
+
+        return detalleCompraEncontrado;
+    }
+
+
+    @Override
+    public boolean actualizarDetalleCompra(int numeroDetalle, String productoComprado, int cantidadCompra, double valorUnidad, double valorTotal, String codigoCompra, String referencia) {
+        return false;
+    }
+
+    @Override
+    public void eliminarDetalleCompra(int numeroDetalle) {
+    }
+
+    /* Método para mostrar una prenda por la referencia */
+    @Override
+    public Producto mostrarProducto(String referencia) {
+        Producto productoEncontrado = null;
+        for (Producto producto : getListaProductos()) {
+            if (producto.getReferencia().equalsIgnoreCase(referencia)) {
+                productoEncontrado = producto;
+                System.out.println(producto.toString());
+                break;
+            }
+        }
+        return productoEncontrado;
+    }
+
+    public List<Cliente> obtenerClientes() {
+        return getListaClientes();
+    }
+
+    public List<Empleado> obtenerEmpleados() {
+        return getListaEmpleados();
+    }
+
+    public List<Producto> obtenerProductos() {
+        return getListaProductos();
+    }
+
+    public void mostrarInformacionClientes(){
+        for (Cliente cliente : getListaClientes()) {
+            System.out.println(cliente.toString());
+        }
+    }
+    public void mostrarInformacionEmpleados(){
+        for (Empleado empleado : getListaEmpleados()) {
+            System.out.println(empleado.toString());
+        }
+    }
+
+    public void mostrarInformacionProductos(){
+        for (Producto producto : getListaProductos()) {
+            System.out.println(producto.toString());
+        }
+    }
+}
